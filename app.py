2c3993585a85a695e054d87c2f00ef393f52c81a
MindCrawler099 <botslayer466@gmail.com>
2023-01-02 22:18:57 +0000

    Added request error catching and checks
    Added try and catch blocks and error raising to search_user_details

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import ttk
 
 class App():
@@ -20,18 +21,27 @@ class App():
         self.check_button.grid(row=4,column=0,columnspan=3,padx=30,pady=10)
 
     def search_user_details(self,username):
+        username = username.lower()
         user_homepage_url = f'https://www.deviantart.com/{username}'
-        user_homepage_source_code = requests.get(user_homepage_url).text
+        response = requests.get(user_homepage_url)
+        response.raise_for_status
+        try:
+            if(response.status_code==200):
+                user_homepage_source_code = response.text
         
-        soup = BeautifulSoup(markup=user_homepage_source_code,features='lxml')
-        number_of_watchers = soup.select(selector='.bdzzI span')[0]
-        print(number_of_watchers.text)
-        number_of_page_views = soup.select(selector='.bdzzI ._1QXgq')[0]
-        print(number_of_page_views.text)
-        number_of_deviations = soup.select(selector='.bdzzI span')[1]
-        print(number_of_deviations.text)
+                soup = BeautifulSoup(markup=user_homepage_source_code,features='lxml')
+                number_of_watchers = soup.select(selector='.bdzzI span')[0]
+                print(number_of_watchers.text)
+                number_of_page_views = soup.select(selector='.bdzzI ._1QXgq')[0]
+                print(number_of_page_views.text)
+                number_of_deviations = soup.select(selector='.bdzzI span')[1]
+                print(number_of_deviations.text)
 
-        self.show_user_details(number_of_watchers.text,number_of_page_views.text,number_of_deviations.text)
+                self.show_user_details(number_of_watchers.text,number_of_page_views.text,number_of_deviations.text)
+
+                messagebox.showerror('App Error','Sorry but your response could not be resolved. Please try again later')
+        except:
+            messagebox.showerror('App Error','Request could not be made at the moment. Please check if you have internet access')
 
     def show_user_details(self,no_watchers,no_page_views,no_deviations):
         self.user_details_label = ttk.Label(self.user_frame,text=f'User Name: {no_watchers}\nPage Views: {no_page_views}\n Deviations: {no_deviations}')
